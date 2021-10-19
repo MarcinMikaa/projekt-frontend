4df3a6509b291d70ba5b767ef9ebc093c9dce32d
Marcin <marcin.mika123@gmail.com>
2021-10-19 15:18:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComp from "./components/NavbarComp/NavbarComp";
+import Footer from "./components/Footer/Footer";
+import MainPage from "./pages/MainPage/MainPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavbarComp />
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/register" exact component={RegisterPage} />
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import "./MainPage.css";
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <h1>Main Page</h1>
+    </div>
+  );
+}
+
+export default MainPage;
