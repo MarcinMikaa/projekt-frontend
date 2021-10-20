d3c78d2679385a8b4d5794bd2d630b3171379774
Marcin <marcin.mika123@gmail.com>
2021-10-20 09:56:49 +0200

    Initial commit

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,8 +1,20 @@
 import "./MainPage.css";
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import ShoeCard from "../../components/ShoeCard/ShoeCard";
 
 function MainPage() {
+  const [shoes, setShoes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setShoes(data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
   return (
     <div className="main-page">
       <h1>INCOMING DROPS</h1>
