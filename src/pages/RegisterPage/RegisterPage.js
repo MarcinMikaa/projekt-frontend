15ad8199894afb9823376b9f72c657641f359585
Marcin <marcin.mika123@gmail.com>
2021-10-20 22:32:54 +0200

    Register Done

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,31 @@
 import "./RegisterPage.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function RegisterPage() {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  const register = () => {
+    axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/register",
+    }).then((res) => console.log(res));
+  };
+
   return (
     <div className="register-page">
       <h1>Register Page</h1>
+      <div>
+        <input placeholder="username" onChange={(e) => setRegisterUsername(e.target.value)} />
+        <input placeholder="password" onChange={(e) => setRegisterPassword(e.target.value)} />
+        <button onClick={register}>Register</button>
+      </div>
     </div>
   );
 }
