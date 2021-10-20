5c6254b1dc767934a39c6785dc5f923cdb7bcc31
Marcin <marcin.mika123@gmail.com>
2021-10-20 08:18:33 +0200

    Main Page Template Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,40 @@
 import "./Footer.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Nav, Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="rows">
-        <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link as={Link} to="/">
-            <p>Home Page</p>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/favorite" className="footer-link">
-            <p>Favorite</p>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/login" className="footer-link">
-            <p>Login</p>
-          </Nav.Link>
-          <Nav.Link as={Link} to="/register" className="footer-link">
-            <p>Register</p>
-          </Nav.Link>
-        </Nav>
-        <div className="contact">
-          <h3>Contact</h3>
-          <p>sneakers-release@gmail.com</p>
-        </div>
-        <img src={logo} className="d-inline-block align-top" alt="Logo" />
-      </div>
+      <Container className="footer-container">
+        <Row>
+          <Col md="4">
+            <Nav defaultActiveKey="/home" className="flex-column">
+              <Nav.Link as={Link} to="/">
+                <p>Home Page</p>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/favorite" className="footer-link">
+                <p>Favorite</p>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login" className="footer-link">
+                <p>Login</p>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register" className="footer-link">
+                <p>Register</p>
+              </Nav.Link>
+            </Nav>
+          </Col>
+          <Col md="4">
+            <div className="contact">
+              <h3>Contact</h3>
+              <p>sneakers-release@gmail.com</p>
+            </div>
+          </Col>
+          <Col md="4">
+            <img src={logo} className="d-inline-block align-top" alt="Logo" />
+          </Col>
+        </Row>
+      </Container>
       <div className="copyrights">
         <p>Copyright © Sneakers Release 2021</p>
       </div>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,58 +1,17 @@
 import "./MainPage.css";
-import { Container, Row, Col } from "react-bootstrap";
-import yeezy from "../../images/yeezy-cloud-white.png";
-import jordan from "../../images/jordan-bred.jpg";
-import airforce from "../../images/air-force-one-offwhite.jpg";
+import { Container, Row } from "react-bootstrap";
+import ShoeCard from "../../components/ShoeCard/ShoeCard";
 
 function MainPage() {
   return (
     <div className="main-page">
       <h1>INCOMING DROPS</h1>
 
-      <Container>
-        <Row>
-          <Col xs={{ order: 4 }}>
-            <img src={yeezy} alt="logo" width="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={jordan} alt="logo" height="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={airforce} alt="logo" height="200" />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={{ order: 4 }}>
-            <img src={yeezy} alt="logo" width="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={jordan} alt="logo" height="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={airforce} alt="logo" height="200" />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={{ order: 4 }}>
-            <img src={yeezy} alt="logo" width="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={jordan} alt="logo" height="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={airforce} alt="logo" height="200" />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={{ order: 4 }}>
-            <img src={yeezy} alt="logo" width="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={jordan} alt="logo" height="200" />
-          </Col>
-          <Col xs={{ order: 4 }}>
-            <img src={airforce} alt="logo" height="200" />
-          </Col>
+      <Container className="main-page-container">
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 6 }).map((_, idx) => (
+            <ShoeCard />
+          ))}
         </Row>
       </Container>
     </div>
