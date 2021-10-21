034b1eab9d0867fc97392b09ba40789d361c9598
Marcin <marcin.mika123@gmail.com>
2021-10-21 22:35:43 +0200

    ShoePage Update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer/Footer";
 import MainPage from "./pages/MainPage/MainPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import ShoeDetail from "./pages/ShoeDetail/ShoeDetail";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" exact component={MainPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/login" component={LoginPage} />
+          <Route path="/shoe/:id" exact component={ShoeDetail} />
         </Switch>
         <Footer />
       </div>
--- src/pages/ShoeDetail/ShoeDetail.js
+++ src/pages/ShoeDetail/ShoeDetail.js
@@ -5,12 +5,21 @@ function ShoeDetail() {
   const [shoeDetail, setShoeDetail] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/shoes");
-  });
+    fetch("http://localhost:4000/shoe/:id")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // setShoeDetail(data);
+        // console.log(shoeDetail);
+      })
+      .catch((e) => console.error(e));
+  }, []);
 
-  return <div className="shoe-detail">
-    
-  </div>;
+  return (
+    <div className="shoe-detail">
+      <h1>{shoeDetail.model}</h1>
+    </div>
+  );
 }
 
 export default ShoeDetail;
