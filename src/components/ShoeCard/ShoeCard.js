3c25ae73c14ec4dfdf9dbb2284b1a96941add20e
Marcin <marcin.mika123@gmail.com>
2021-10-21 17:59:48 +0200

    RegisterLoginPages Done

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,14 +1,14 @@
 import "./ShoeCard.css";
 import { Col, Card } from "react-bootstrap";
 
-function ShoeCard({ url, brand, model }) {
+function ShoeCard({ url, brand, model, price }) {
   return (
     <Col>
       <Card>
         <Card.Img variant="top" src={url} />
         <Card.Body>
-          <Card.Title>{brand}</Card.Title>
-          <Card.Text>{model}</Card.Text>
+          <Card.Title>{model}</Card.Title>
+          <Card.Text>{price} USD</Card.Text>
         </Card.Body>
       </Card>
     </Col>
